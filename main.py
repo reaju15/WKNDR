ac5740c36253658d1a40f1529f2da0149be76f09
Reaj <reaju15@live.com>
2022-05-03 11:27:26 -0400

    new update

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ def thankyou():
     return render_template("Thank-You-Page.html")
      
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
